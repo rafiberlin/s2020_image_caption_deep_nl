5122da2d849968aa639eaab3b95592a4d9f1ca4d
Alexander Koch <kochalexander@gmx.net>
2020-06-20 19:34:42 +0200

    Fix GloVe loading, add output directory creation

--- src/main.py
+++ src/main.py
@@ -12,10 +12,10 @@ from timeit import default_timer as timer
 import gensim
 # own modules
 import model
-import preprocessing
+import preprocessing as prep
 
-DATASET_FILE_PATHS_CONFIG = "./dataset_file_args.json"
-HYPER_PARAMETER_CONFIG = "./hyper_parameters.json"
+DATASET_FILE_PATHS_CONFIG = "dataset_file_args.json"
+HYPER_PARAMETER_CONFIG = "hyper_parameters.json"
 N_EPOCHS = 120
 LEARNING_RATE = 0.01
 REPORT_EVERY = 5
@@ -31,36 +31,43 @@ END_WORD = "<END>"
 IMAGE_SIZE = 320
 
 def main():
-    file_args = preprocessing.read_json_config(DATASET_FILE_PATHS_CONFIG)
-    hyper_parameters = preprocessing.read_json_config(HYPER_PARAMETER_CONFIG)
+    file_args = prep.read_json_config(DATASET_FILE_PATHS_CONFIG)
+    hyper_parameters = prep.read_json_config(HYPER_PARAMETER_CONFIG)
     device = hyper_parameters["device"]
     if not torch.cuda.is_available():
         device = "cpu"
 
-    cleaned_captions = preprocessing.create_list_of_captions_and_clean(file_args["annotations"],
-                                                                       file_args["train"]["capt"])
+    cleaned_captions = prep.create_list_of_captions_and_clean(file_args, "train")
     c_vectorizer = model.CaptionVectorizer.from_dataframe(cleaned_captions)
     padding_idx = c_vectorizer.caption_vocab._token_to_idx[PADDING_WORD]
 
     if hyper_parameters["use_glove"]:
-        glove_model = gensim.models.KeyedVectors.load_word2vec_format('data/glove.6B.100d.bin.word2vec')
-        embedding = nn.Embedding.from_pretrained(glove_model.wv)
+        ## TODO: pass embedding to model
+        print("Loading glove vectors...")
+        glove_path = os.path.join(file_args['root'], file_args['glove_embedding'])
+        glove_model = gensim.models.KeyedVectors.load_word2vec_format(glove_path, binary=True)
+        glove_weights = torch.FloatTensor(glove_model.vectors)
+        embedding = nn.Embedding.from_pretrained(glove_weights)
         embedding.weight.requires_grad = False
 
-    image_dir = file_args["train"]["img"]
-    caption_file_path = os.path.join(file_args["annotations"], file_args["train"]["capt"])
-    rgb_stats = preprocessing.read_json_config(file_args["rgb_stats"])
-    stats_rounding = hyper_parameters["rounding"]
-    rgb_mean = tuple([round(m, stats_rounding) for m in rgb_stats["mean"]])
-    rgb_sd = tuple([round(s, stats_rounding) for s in rgb_stats["mean"]])
+    image_dir = os.path.join(file_args['root'], file_args['train'])
+
+    caption_file_path = prep.get_captions_path(file_args, file_args['train'])
+    print("Image dir:", image_dir)
+    print("Caption file path:", caption_file_path)
+
+    #rgb_stats = prep.read_json_config(file_args["rgb_stats"])
+    #stats_rounding = hyper_parameters["rounding"]
+    #rgb_mean = tuple([round(m, stats_rounding) for m in rgb_stats["mean"]])
+    #rgb_sd = tuple([round(s, stats_rounding) for s in rgb_stats["mean"]])
     # TODO create a testing split, there is only training and val currently...
     coco_train_set = dset.CocoDetection(root=image_dir,
                                         annFile=caption_file_path,
-                                        transform=transforms.Compose([preprocessing.CenteringPad(),
+                                        transform=transforms.Compose([prep.CenteringPad(),
                                                                       transforms.Resize((640, 640)),
                                                                       # transforms.CenterCrop(IMAGE_SIZE),
-                                                                      transforms.ToTensor(),
-                                                                      transforms.Normalize(rgb_mean, rgb_sd)])
+                                                                      transforms.ToTensor()])
+                                                                      #transforms.Normalize(rgb_mean, rgb_sd)])
                                         )
 
     coco_dataset_wrapper = model.CocoDatasetWrapper(coco_train_set, c_vectorizer)
@@ -68,7 +75,18 @@ def main():
     train_loader = torch.utils.data.DataLoader(coco_dataset_wrapper, batch_size)
 
     vocabulary_size = len(c_vectorizer.caption_vocab)
-    model_path = file_args["model_storage_dir"]
+
+    ## Generate output folder if non-existent
+    model_dir = file_args["model_storage"]
+    if not os.path.isdir(model_dir):
+        try:
+            os.mkdir(model_dir)
+        except OSError:
+            print(f"Creation of the directory {model_dir} failed")
+    model_path = os.path.join(model_dir, file_args["model_name"])
+    print("Model save path:", model_path)
+
+    ## Training start
     network = model.LSTMModel(EMBEDDING_DIM, vocabulary_size, HIDDEN_DIM_RNN, HIDDEN_DIM_CNN, padding_idx).to(device)
     start_training = True
     if os.path.isfile(model_path):
