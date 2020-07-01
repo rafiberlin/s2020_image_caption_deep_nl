d958ae9defed1a38b900b41ca345764b8a9e05ed
R. Latif <rafi@six.de>
2020-07-01 19:13:02 +0200

    Remove wrong path

--- utils/glove_conv.py
+++ utils/glove_conv.py
@@ -5,20 +5,20 @@ from gensim.models import KeyedVectors
 
 
 print("Converting glove txt -> word2vec txt...")
-glove_input_file = '../data/glove.6B.100d.txt'
-word2vec_output_file = '../data/glove.6B.100d.txt.word2vec'
+glove_input_file = './data/glove.6B.100d.txt'
+word2vec_output_file = './data/glove.6B.100d.txt.word2vec'
 glove2word2vec(glove_input_file, word2vec_output_file)
 print("Converting word2vec txt -> word2vec bin...")
 w2v = KeyedVectors.load_word2vec_format(word2vec_output_file, binary=False)
-w2v.save_word2vec_format('../data/glove.6B.100d.bin.word2vec', binary=True)
+w2v.save_word2vec_format('./data/glove.6B.100d.bin.word2vec', binary=True)
 
 print("Converting glove txt -> word2vec txt...")
-glove_input_file = '../data/glove.6B.300d.txt'
-word2vec_output_file = '../data/glove.6B.300d.txt.word2vec'
+glove_input_file = './data/glove.6B.300d.txt'
+word2vec_output_file = './data/glove.6B.300d.txt.word2vec'
 glove2word2vec(glove_input_file, word2vec_output_file)
 print("Converting word2vec txt -> word2vec bin...")
 w2v = KeyedVectors.load_word2vec_format(word2vec_output_file, binary=False)
-w2v.save_word2vec_format('../data/glove.6B.300d.bin.word2vec', binary=True)
+w2v.save_word2vec_format('./data/glove.6B.300d.bin.word2vec', binary=True)
 
 print("Converting glove txt -> word2vec txt...")
 glove_input_file = './data/glove.6B.50d.txt'
