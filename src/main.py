ef3be0a960ab54c53be8238d50581be6cae67691
R. L <latif@six.de>
2020-07-24 00:51:34 +0200

    Add a Jupyter Notebook. TODO: display some predictions along with the references  and the images in the Jupyter notebook

--- src/main.py
+++ src/main.py
@@ -272,11 +272,6 @@ def main():
         print("CUDA GPU is available", "Number of machines:", torch.cuda.device_count())
 
     prep.set_seed_everywhere(SEED)
-    img_list = None
-    if hparams["debug"]:
-        # The image list help to retrieve only captions corresponding to break_training_loop_percentage in hparams. Helps with memory issues...
-        img_list = prep.get_current_images_id(hparams, trainset_name)
-    annotation_without_punctuation = hparams["annotation_without_punctuation"]
     cleaned_captions = prep.get_captions(hparams, trainset_name)
     cutoff_for_unknown_words = hparams["cutoff"]
     c_vectorizer = model.CaptionVectorizer.from_dataframe(cleaned_captions, cutoff_for_unknown_words)
